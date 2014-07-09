258aff993c2ad4beb8892196606c844d86c5e67f
george-quaye-sap-com <george.quaye@sap.com>
2014-07-09 15:50:04 -0700

    Update BaseGroovyMojo.groovy
    copy-maven-plugin is incompatible with Maven 3.1

--- maven-common/src/main/groovy/com/github/goldin/plugins/common/BaseGroovyMojo.groovy
+++ maven-common/src/main/groovy/com/github/goldin/plugins/common/BaseGroovyMojo.groovy
@@ -17,10 +17,10 @@ import org.codehaus.plexus.DefaultPlexusContainer
 import org.gcontracts.annotations.Ensures
 import org.gcontracts.annotations.Requires
 import org.slf4j.LoggerFactory
-import org.sonatype.eclipse.RepositorySystem
-import org.sonatype.eclipse.RepositorySystemSession
-import org.sonatype.eclipse.repository.RemoteRepository
-import org.sonatype.eclipse.resolution.ArtifactRequest
+import org.eclipse.aether.RepositorySystem
+import org.eclipse.aether.RepositorySystemSession
+import org.eclipse.aether.repository.RemoteRepository
+import org.eclipse.aether.resolution.ArtifactRequest
 import org.springframework.util.ReflectionUtils
 import java.lang.reflect.Field
 import java.lang.reflect.Modifier
